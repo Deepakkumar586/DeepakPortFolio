31a0bb03648c3ce8ac9b71738e801167c4ec3820
Deepak <aryanrajj1234@gmail.com>
2025-01-07 16:05:04 +0530

    add some code in frontend

--- Frontend/src/components/cards/ProjectCard.jsx
+++ Frontend/src/components/cards/ProjectCard.jsx
@@ -3,13 +3,13 @@ import styled from "styled-components";
 
 const Card = styled.div`
   width: 330px;
-  height: 490px;
+  height: 400px;
   background-color: ${({ theme }) => theme.card};
   cursor: pointer;
   border-radius: 10px;
   box-shadow: 0 0 12px 4px rgba(0, 0, 0, 0.4);
   overflow: hidden;
-  padding: 26px 20px;
+  padding: 20px;
   display: flex;
   flex-direction: column;
   gap: 14px;
@@ -20,13 +20,7 @@ const Card = styled.div`
     filter: brightness(1.1);
   }
 `;
-const Image = styled.img`
-  width: 100%;
-  height: 180px;
-  background-color: ${({ theme }) => theme.white};
-  border-radius: 10px;
-  box-shadow: 0 0 16px 2px rgba(0, 0, 0, 0.3);
-`;
+
 const Tags = styled.div`
   width: 100%;
   display: flex;
@@ -35,23 +29,26 @@ const Tags = styled.div`
   gap: 8px;
   margin-top: 4px;
 `;
+
 const Tag = styled.div`
   font-size: 12px;
   font-weight: 400;
   color: ${({ theme }) => theme.primary};
   background-color: ${({ theme }) => theme.primary + 15};
-  padding: 2px 8px;
-  border-radius: 10px;
+  padding: 4px 10px;
+  border-radius: 20px;
 `;
+
 const Details = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 0px;
+  gap: 10px;
   padding: 0px 2px;
 `;
+
 const Title = styled.div`
-  font-size: 20px;
+  font-size: 22px;
   font-weight: 600;
   color: ${({ theme }) => theme.text_secondary};
   overflow: hidden;
@@ -59,9 +56,9 @@ const Title = styled.div`
   max-width: 100%;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-  overflow: hidden;
   text-overflow: ellipsis;
 `;
+
 const Date = styled.div`
   font-size: 12px;
   margin-left: 2px;
@@ -71,6 +68,7 @@ const Date = styled.div`
     font-size: 10px;
   }
 `;
+
 const Description = styled.div`
   font-weight: 400;
   color: ${({ theme }) => theme.text_secondary + 99};
@@ -82,28 +80,13 @@ const Description = styled.div`
   -webkit-box-orient: vertical;
   text-overflow: ellipsis;
 `;
-const Members = styled.div`
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-`;
-const Avatar = styled.img`
-  width: 38px;
-  height: 38px;
-  border-radius: 50%;
-  margin-left: -10px;
-  background-color: ${({ theme }) => theme.white};
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  border: 3px solid ${({ theme }) => theme.card};
-`;
 
 const ProjectCard = ({ project, setOpenModal }) => {
   return (
     <Card onClick={() => setOpenModal({ state: true, project: project })}>
-      <Image src={project?.image} />
       <Tags>
         {project.tags?.map((tag, index) => (
-          <Tag>{tag}</Tag>
+          <Tag key={index}>{tag}</Tag>
         ))}
       </Tags>
       <Details>
@@ -111,11 +94,6 @@ const ProjectCard = ({ project, setOpenModal }) => {
         <Date>{project.date}</Date>
         <Description>{project.description}</Description>
       </Details>
-      <Members>
-        {project.member?.map((member) => (
-          <Avatar src={member.img} />
-        ))}
-      </Members>
     </Card>
   );
 };
